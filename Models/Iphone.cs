894a776f8c3f3439d250d43de04c343e26dbea71
ViniciusSantana72 <130595003+ViniciusSantana72@users.noreply.github.com>
2023-12-30 04:44:29 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,41 @@
-namespace DesafioPOO.Models
+public class iPhone : Smartphone
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Construtor
+    public iPhone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+    }
+
+    // Implementação dos métodos abstratos
+    public override void Ligar()
+    {
+        Console.WriteLine("iPhone ligado.");
+    }
+
+    public override void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação no iPhone.");
+    }
+
+    public override void InstalarAplicativo(string nome)
+    {
+        Console.WriteLine($"Instalando aplicativo {nome} no iPhone.");
+    }
+}
+
+// Classe principal para testar o código
+class Program
+{
+    static void Main()
+    {
+        // Exemplo de uso
+        Smartphone nokia = new Nokia("123456789", "Nokia 7", "IMEI123", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Smartphone iphone = new iPhone("987654321", "iPhone 12", "IMEI456", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
     }
 }
